f998033d59814bc1fb070800ec471c31f9d0728b
Christian <christianbarrios@ufm.edu>
2024-08-12 18:00:58 -0600

    Primer Commit

---
+++ lab1.R
@@ -0,0 +1,14 @@
+library(readr)
+library(dplyr)
+
+tienda <- read_csv("C:/Users/barri/Documents/UFM/2024/Segundo Semestre/Data Product/flexdashboards/Lab1/tienda.csv")
+View(tienda)
+
+producto_mas_rentable <- tienda %>%
+  filter(Profit == max(Profit))
+
+producto_menos_rentable <- tienda %>%
+  filter(Profit == min(Profit))
+
+producto_mas_rentable
+producto_menos_rentable
